e64dbb891d92f2fd16e08781e11e8b68619137d9
Mari-Vazquez <marianela.beatriz.vazquez@gmail.com>
2023-09-14 21:01:46 -0300

    Creamos un objeto MateriaData

--- universidadUlp_transversal/src/universidadulp_transversal/UniversidadUlp_transversal.java
+++ universidadUlp_transversal/src/universidadulp_transversal/UniversidadUlp_transversal.java
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package universidadulp_transversal;
+package UniversidadUlp_transversal;
 
-import AccesoADatos.AlumnoDate;
+
+import AccesoADatos.AlumnoData;
 import AccesoADatos.Conexion;
+import AccesoADatos.InscripcionData;
+import AccesoADatos.MateriaData;
 import Entidades.Alumno;
+import Entidades.Inscripcion;
+import Entidades.Materia;
 import java.sql.Connection;
 
 
@@ -23,7 +28,7 @@ public class UniversidadUlp_transversal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//Connection con=Conexion.getConexion();
+Connection con=Conexion.getConexion();
 //
 //Alumno Juan=new Alumno (15,456123852,"Torres","Pedro", LocalDate.of(1980, 4, 25),true);
 //AlumnoData alu=new AlumnoData();
@@ -33,15 +38,30 @@ public class UniversidadUlp_transversal {
 //Alumno alumnoEncontrado=alu.buscarAlumnoPorDni(987654321);
 //    
 //        System.out.println("apellido "+alumnoEncontrado.getApellido());
-        AlumnoDate alu = new AlumnoDate();
-        for (Alumno alumno : alu.listarAlumnos()) {
-
-            System.out.println(alumno.getDni());
-            System.out.println(alumno.getApellido());
-            System.out.println(alumno.getNombre());
-            System.out.println(alumno.getFechaNacimiento());
-        }
-
+//        AlumnoDate alu = new AlumnoDate();
+//        for (Alumno alumno : alu.listarAlumnos()) {
+//
+//            System.out.println(alumno.getDni());
+//            System.out.println(alumno.getApellido());
+//            System.out.println(alumno.getNombre());
+//            System.out.println(alumno.getFechaNacimiento());
+//        }
+//
+//      AlumnoData ad=new AlumnoData();
+//      MateriaData md=new MateriaData ();
+//      InscripcionData id=new InscripcionData ();
+//      
+//      Alumno funes = ad.buscarAlumno(5);
+//      Materia lengua= md.buscarMateria (2);
+//      Inscripcion insc=new Inscripcion (funes,lengua,8);
+//
+//    }
+//  
+        Materia literatura = new Materia ("literatura", 11 , true);
+        MateriaData mat =new MateriaData();
+        mat.guardarMateria(literatura);
+        
+        
     }
-
 }
+  
